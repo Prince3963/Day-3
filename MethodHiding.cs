94fe6f12df9ca13c73973805103a80b62b197e81
Prince3963 <princepatel3963@gmail.com>
2026-01-28 15:31:48 +0530

    22

---
+++ MethodHiding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_30
+{
+    public class MethodHiding
+    {
+        public void Display()
+        {
+            Console.WriteLine("Base Class");
+        }
+    }
+
+    public class DerivedClass() : MethodHiding
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Derived Class");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,29 +2,45 @@
 
 using _21_30;
 
-Student student = new Student();
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Student student = new Student();
 
-Console.WriteLine();
+        //Console.WriteLine();
 
-Console.WriteLine("-----------------------------------------");
-Console.WriteLine("Assigning values to Student class object properties");
-student.name = "John";
-student.age = 20;
-student.country = "USA";
-student.Marks = 95;
-Console.WriteLine();
-student.Display();
+        //Console.WriteLine("-----------------------------------------");
+        //Console.WriteLine("Assigning values to Student class object properties");
+        //student.name = "John";
+        //student.age = 20;
+        //student.country = "USA";
+        //student.Marks = 95;
+        //Console.WriteLine();
+        //student.Display();
 
-Console.WriteLine("-----------------------------------------");
+        //Console.WriteLine("-----------------------------------------");
 
-Teacher teacher = new Teacher();
-Console.WriteLine();
-Console.WriteLine("-----------------------------------------");
-Console.WriteLine("Assigning values to Teacher class object properties");
-teacher.name = "Sneha";
-teacher.age = 26;
-teacher.country = "India";
-teacher.Salary = 50000;
-Console.WriteLine();
-teacher.Display();
-Console.WriteLine("-----------------------------------------");
+        //Teacher teacher = new Teacher();
+        //Console.WriteLine();
+        //Console.WriteLine("-----------------------------------------");
+        //Console.WriteLine("Assigning values to Teacher class object properties");
+        //teacher.name = "Sneha";
+        //teacher.age = 26;
+        //teacher.country = "India";
+        //teacher.Salary = 50000;
+        //Console.WriteLine();
+        //teacher.Display();
+        Console.WriteLine("-----------------------------------------");
+
+
+        MethodHiding m = new MethodHiding();
+        DerivedClass d = new DerivedClass();
+        MethodHiding md = new DerivedClass();
+
+
+        m.Display();
+        d.Display();
+        md.Display();
+    }
+}
