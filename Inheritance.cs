53dd3b38dd1c104b89d5a79a526263ce0d964113
Prince3963 <princepatel3963@gmail.com>
2026-01-28 11:23:57 +0530

    Add project files.

---
+++ Inheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_30
+{
+    //Parent class
+    public class InheritanceClass
+    {
+        public string? name;
+        public int age;
+        public string? country;
+        public InheritanceClass()
+        {
+            Console.WriteLine("This is InheritClass class Constructor");
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"name = {name}, age = {age}, country = {country}");
+        }
+    }
+
+    //Child class
+    public class  Student : InheritanceClass
+    {
+        public Student()
+        {
+            Console.WriteLine("This is Student class Constructor");
+        }
+        public int Marks;
+    }
+
+    public class Teacher : InheritanceClass
+    {
+        public Teacher()
+        {
+            Console.WriteLine("This is Teacher class Constructor");
+        }
+        public int Salary;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using _21_30;
+
+Student student = new Student();
+
+Console.WriteLine();
+
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("Assigning values to Student class object properties");
+student.name = "John";
+student.age = 20;
+student.country = "USA";
+student.Marks = 95;
+Console.WriteLine();
+student.Display();
+
+Console.WriteLine("-----------------------------------------");
+
+Teacher teacher = new Teacher();
+Console.WriteLine();
+Console.WriteLine("-----------------------------------------");
+Console.WriteLine("Assigning values to Teacher class object properties");
+teacher.name = "Sneha";
+teacher.age = 26;
+teacher.country = "India";
+teacher.Salary = 50000;
+Console.WriteLine();
+teacher.Display();
+Console.WriteLine("-----------------------------------------");
